6b9e455384e942077afef0e0c99e68bc0c8baf00
Maryfer Ramos Bustos <62928399+ramosbustosmaryfer@users.noreply.github.com>
2024-03-06 01:23:26 -0600

    velocidades

--- Videojuego de conducción/Assets/Script/controller.cs
+++ Videojuego de conducción/Assets/Script/controller.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class controller : MonoBehaviour
 {
-    public int Freanar = 1000;
+    public int Freanar = 500;
     public WheelCollider Rueda1;
     public WheelCollider Rueda2;
     public WheelCollider Rueda3;
@@ -12,17 +12,28 @@ public class controller : MonoBehaviour
     public Transform Llantas1;
     public Transform Llantas2;
     public int Velocity = 1000;
+    public float velocityAct;
+    public int VelocityMax = 2000;
 
     void Update()
     {
+        velocityAct = (2 * Mathf.PI * Rueda1.radius) * Rueda1.rpm * 60 / 100;
         Llantas1.localEulerAngles = new Vector3(0, Rueda3.steerAngle, 0);
         Llantas2.localEulerAngles = new Vector3(0, Rueda4.steerAngle, 0);
     }
 
     void FixedUpdate()
     {
-        Rueda1.motorTorque = Velocity * Input.GetAxis("Vertical");
-        Rueda2.motorTorque = Velocity * Input.GetAxis("Vertical");
+        if (velocityAct < VelocityMax)
+        {
+            Rueda1.motorTorque = Velocity * Input.GetAxis("Vertical");
+            Rueda2.motorTorque = Velocity * Input.GetAxis("Vertical");
+        }
+        else
+        {
+            Rueda1.motorTorque = 0;
+            Rueda2.motorTorque = 0;
+        }
         Rueda3.steerAngle = -40 * Input.GetAxis("Horizontal"); //rotacion
         Rueda4.steerAngle = -40 * Input.GetAxis("Horizontal");
 
