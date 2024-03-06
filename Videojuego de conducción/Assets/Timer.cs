9478acbc208bb5b91809595091a41695c76934ec
Cindy-001 <100329024+Cindy-001@users.noreply.github.com>
2024-03-05 21:29:17 -0600

    pepe3

---
+++ Videojuego de conducción/Assets/Timer.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using TMPro;
+
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private TMP_Text timerText;
+
+    private float timeElapsed;
+    private int minutes, seconds, cents; 
+       
+
+
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        minutes = (int)(timeElapsed / 60f);
+        seconds = (int)(timeElapsed - minutes * 60f);
+        cents = (int)((timeElapsed - (int)timeElapsed) * 100f);
+        timerText.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, cents);
+    }
+}
