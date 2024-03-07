2981292ddc1ca377802da1744cc3a5805ccd45f2
Maryfer Ramos Bustos <62928399+ramosbustosmaryfer@users.noreply.github.com>
2024-03-06 20:19:17 -0600

    countdown

---
+++ Videojuego de conducción/Assets/CountdownController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownController : MonoBehaviour
+{
+    public int countdownTime;
+    public Text countdownDisplay;
+
+    private void Start()
+    {
+        StartCorutine(CountdownToStar());
+    }
+
+    IEnumerator CountdownToStar()
+    {
+        while (countdownTime > 0)
+        {
+            countdownDisplay.text = countdownTime.ToString();
+            yield return new WaitForSeconds(1f);
+            countdownTime--;
+        }
+
+        countdownDisplay.text = "GO!";
+        GameController.instance.BeginGame();
+
+        yield return new WaitForSeconds(1f);
+        countdownDisplay.gameObject.SetActive(false);
+    }
+}
