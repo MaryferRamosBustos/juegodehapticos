a70f4e713dad95a5e027f3acd9417f1debb23376
Maryfer Ramos Bustos <62928399+ramosbustosmaryfer@users.noreply.github.com>
2024-03-06 23:14:52 -0600

    oof

---
+++ Videojuego de conducción/Assets/destruir.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destruir : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
